2ad987737c1512b238a51ffcdd3ec566db44c53d
Edward Lüderitz <edluder@gmail.com>
2018-05-30 11:19:33 -0300

    alteração feita

--- exemplo-04.php
+++ exemplo-04.php
@@ -3,9 +3,19 @@
 $nome = (int)$_GET["a"];
 $nome1 = (int)$_GET["b"];
 
-var_dump($nome);
-var_dump($nome1);
+/*var_dump($nome);
+var_dump($nome1);*/
 
-//http://localhost/projeto/php/exemplo-04.php?a=123
+//http://localhost/projeto/php/exemplo-04.php?a=123&b=456
+
+//pegar o ip do usuário
+
+//$ip = $_SERVER["REMOTE_ADDR"];
+
+$ip = $_SERVER["SCRIPT_NAME"];
+
+echo $ip;
+
+
 
 ?>
