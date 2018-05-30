fb51ca752d95ce5f204adb02ccf2cc43fe3c8e23
Edward Lüderitz <edluder@gmail.com>
2018-05-30 15:48:22 -0300

    aula de upper, lower, first, word

---
+++ string-02.php
@@ -0,0 +1,30 @@
+<?php
+
+$nome = "joao rangel";
+
+$nome = strtoupper($nome);
+
+echo $nome;
+
+echo "<br>";
+
+$nome = strtolower($nome);
+
+echo $nome;
+
+echo "<br>";
+
+$nome = ucfirst($nome);
+
+echo $nome;
+
+echo "<br>";
+
+$nome = ucwords($nome);
+
+echo $nome;
+
+echo "<br>";
+
+
+?>
