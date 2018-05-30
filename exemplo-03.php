d9489866bbef774fba60d7801f3b68e25c895843
Edward Lüderitz <edluder@gmail.com>
2018-05-30 10:50:03 -0300

    alteração nos exemplos

--- exemplo-03.php
+++ exemplo-03.php
@@ -1,13 +1,27 @@
-<? php
+<?php
 
 $nome = "Hcode";
 
-$site = 'www.hcode.com.br'
+$site = 'www.hcode.com.br';
 
 $ano = 1990;
 
 $salario = 5500.99;
 
 $bloqueado = false;
+
+//////////////////////////////////////////////////
+
+$frutas = array("abacaxi", "laranja", "manga");
+
+echo $frutas[2];
+
+$nascimento = new DateTime();
+
+var_dump($nascimento);
+
+$arquivo = fopen("exemplo-03.php", "r");
+
+var_dump($arquivo);
 
 ?>
