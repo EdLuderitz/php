fbe251f4691bf8020670db8c669910ff2dc19c5a
Edward Lüderitz <edluder@gmail.com>
2018-05-30 14:51:07 -0300

    operadores

--- exemplo-08.php
+++ exemplo-08.php
@@ -1,5 +1,34 @@
 <?php
 
-	
+$a = 10;
+$b = 2;
+
+echo "A Soma dos números 10 e 2 é ", $a + $b;
+
+echo "</br></br>";
+
+echo "A Subtração dos números 10 e 2 é ", $a - $b;
+
+echo "</br></br>";
+
+echo "O resto da divisão dos números 10 e 2 é ", $a % $b;
+
+echo "</br></br>";
+
+echo "A Multiplicação dos números 10 e 2 é ", $a * $b;
+
+echo "</br></br>";
+
+echo "A Divisão dos números 10 e 2 é ", $a / $b;
+
+echo "</br></br>";
+
+echo "A Exponenciação dos números 10 e 2 é ", $a ** $b;
+
+echo "</br></br>";
+
+
+
+
 
 ?>
