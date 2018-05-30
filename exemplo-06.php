329b547f39f6acbb33e7261157877024b7c99211
Edward Lüderitz <edluder@gmail.com>
2018-05-30 13:43:09 -0300

    novos exemplos

---
+++ exemplo-06.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "hcode";
+
+echo $nome . " mais alguma coisa</br>";
+
+$nome .= " treinamentos";
+
+echo $nome;	
+
+?>
---
+++ exemplo-07.php
@@ -0,0 +1,12 @@
+<?php
+
+$valortotal = 0;
+
+
+$valortotal += 100;
+
+$valortotal += 25;
+
+echo $valortotal;
+
+?>
