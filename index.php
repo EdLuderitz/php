dacff93c517c9f2eb8c79cfae6bf73e2af6a72a3
Edward Lüderitz <edluder@gmail.com>
2018-05-30 09:35:19 -0300

    deletando variaveis

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php 
 
-$nome = "hcode";
+$nome = "joao";
 $anoNascimento = 1998;
 
 echo "$nome $anoNascimento";
+
+unset($nome);
+
+$nome = "maria";
+$anoNascimento = 1999;
+
+echo "</br></br>";
+
+echo "$nome $anoNascimento"
 
 ?>
